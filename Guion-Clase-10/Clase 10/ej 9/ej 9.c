0257f44407549609b12a4a5ad914c7cd77ab9b06
Marco Flores <marcofloresantonio899@gmail.com>
2020-12-13 16:45:28 -0300

    Update ej 9.c

--- Guion-Clase-10/Clase 10/ej 9/ej 9.c
+++ Guion-Clase-10/Clase 10/ej 9/ej 9.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) 
+{
 	int i,n=0,n1=0,resu=0,dea=0,acu=0;
 	printf("Ingrese un numero a multiplicar ");
 	scanf("%d",&n);
