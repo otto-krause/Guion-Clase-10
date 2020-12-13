4828d6c00a502f73a568e4925453b5411dcb3e66
Marco Flores <marcofloresantonio899@gmail.com>
2020-12-13 16:45:33 -0300

    Update ej 10.c

--- Guion-Clase-10/Clase 10/ej 10/ej 10.c
+++ Guion-Clase-10/Clase 10/ej 10/ej 10.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) 
+{
 	int i=1,div;
 	float rt,ac,nd;
 	printf("Ingrese un numero a dividir ");
