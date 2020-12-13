52db33ddcb1f1002914bf870b7184cdbf3de19e2
Marco Flores <marcofloresantonio899@gmail.com>
2020-12-13 17:18:43 -0300

    Update ej 1.c

--- Guion-Clase-10/Clase 10/ej 1/ej 1.c
+++ Guion-Clase-10/Clase 10/ej 1/ej 1.c
@@ -7,14 +7,12 @@ int main(int argc, char *argv[])
  {
 	int i,x,nt;
 	char nm[20],mvp[100],ma[100];
-	for (i=1;i<16;i++)
-	{
+	for (i=1;i<16;i++) {
 		printf("\n Ingrese la nota de el alumno %d ",i);
 		scanf("%d",&nt);
 		printf("\n Ingrese el nombre de el alumno %d ",i);
 		scanf("%s",&nm);
-		if (nt>=8)
-		{
+		if (nt>=8) {
 			strcpy(mvp,nm);
 			strcat(ma,mvp);
 			strcat(ma," ");
